5dfe7ed85bf31a392ceb210b9db933f7f64d2af4
Маколин Илья <makolin.ilya@wb.ru>
2025-05-04 22:24:27 +0300

    hw_8

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +11,37 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errs []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	if e == nil || len(e.errs) == 0 {
+		return ""
+	}
+	sb := strings.Builder{}
+	sb.Grow(124) // можно, конечно, точно посчитать, но это займет время (всякие err.Error() как будто бы долгие)
+	sb.WriteString("2 errors occured:\n")
+	for i := range e.errs {
+		sb.WriteString("\t* " + e.errs[i].Error())
+	}
+	sb.WriteString("\n")
+
+	return strings.Clone(sb.String())
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	if err == nil {
+		return &MultiError{errs: errs}
+	}
+	multErr, ok := err.(*MultiError)
+	if !ok {
+		multErr = &MultiError{}
+		multErr.errs = make([]error, 0, len(errs)+1)
+		multErr.errs = append(multErr.errs, err)
+	}
+	multErr.errs = append(multErr.errs, errs...)
+
+	return multErr
 }
 
 func TestMultiError(t *testing.T) {
